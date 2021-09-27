21b1c2b512241ab8e8bd29a9c59f5eff4f75ed5d
JMoo <49797086+jmoo512@users.noreply.github.com>
2021-09-26 22:14:37 -0500

    some base routes

--- application/cards/routes.py
+++ application/cards/routes.py
@@ -2,3 +2,8 @@ from flask import Blueprint
 from flask import current_app as app
 
 cards = Blueprint('cards', __name__)
+
+@cards.route('/cards/<card_name>')
+def get_by_card_name(card_name):
+
+    return card_name
--- application/sets/routes.py
+++ application/sets/routes.py
@@ -15,4 +15,4 @@ def get_all_sets():
 @sets.route('/sets/<set_name>')
 def get_set_by_set_name(set_name):
 
-    return (set_name)
+    return set_name
