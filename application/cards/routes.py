35ca036f7c5163920bd33dd80b73603a66bd669e
JMoo <49797086+jmoo512@users.noreply.github.com>
2021-09-28 17:45:26 -0500

    cards routes

--- application/cards/routes.py
+++ application/cards/routes.py
@@ -6,6 +6,7 @@ from application.schemas import card_schema, cards_schema
 
 cards = Blueprint('cards', __name__)
 
+####GET CARDS BY CARD NAME
 @cards.route('/cards/<card_name>')
 def get_by_card_name(card_name):
 
@@ -14,3 +15,29 @@ def get_by_card_name(card_name):
     result = cards_schema.dump(q)
 
     return jsonify(result)
+
+
+####GET CARD BY CS ID
+@cards.route('/cards/csid/<cs_id>')
+def get_by_cs_id(cs_id):
+
+    q = Cards.query.filter(Cards.cs_id == cs_id).first_or_404()
+
+    result = card_schema.dump(q)
+
+    return jsonify(result)
+
+
+####GET CARD BY MTGJSON ID
+@cards.route('/cards/mtgjsonid/<mtgjson_id>')
+def get_by_mtgjson_id(mtgjson_id):
+
+    q = Cards.query.filter(Cards.mtgjson_id == mtgjson_id).first_or_404()
+
+    result = card_schema.dump(q)
+
+    return jsonify(result)
+
+
+
+####GET CARD BY SCRYFALL ID
