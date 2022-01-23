1dddc0128c4525d8dc34ec7a42a9f7a847efdb83
jmoo <pauperbattlebox@gmail.com>
2022-01-23 14:08:34 -0600

    proxyfix

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+from werkzeug.middleware.proxy_fix import ProxyFix
 from .extensions import db, migrate, cache, limiter
 
 current_version = '/api/v1/'
@@ -18,6 +18,8 @@ def create_app():
 
 
     with app.app_context():
+
+        app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1)
 
         from .core.routes import core
         from .sets.routes import sets
--- application/extensions.py
+++ application/extensions.py
@@ -3,7 +3,6 @@ from flask_migrate import Migrate
 from flask_caching import Cache
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-
 from os import environ, path
 from dotenv import load_dotenv
 
