4da588af724fb9b09a7d463dbae5a74acb40e7d4
JMoo <49797086+jmoo512@users.noreply.github.com>
2022-02-01 20:43:54 -0600

    remove extraneous classmethod and migrate functionality

--- application/extensions.py
+++ application/extensions.py
@@ -1,5 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_caching import Cache
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
@@ -10,7 +9,7 @@ basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
 
 db = SQLAlchemy()
-migrate = Migrate()
+
 cache = Cache(config = {'CACHE_TYPE': 'RedisCache',
                         'CACHE_REDIS_URL': environ.get('REDISTOGO_URL')})
 limiter = Limiter(key_func=get_remote_address)
