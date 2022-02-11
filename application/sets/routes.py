f03db4eb1d282518e99dace83d71bffee613dadc
jmoo <pauperbattlebox@gmail.com>
2022-02-10 21:53:22 -0600

    lint spacing

--- application/sets/routes.py
+++ application/sets/routes.py
@@ -4,8 +4,7 @@ from flask import jsonify
 
 from application import cache, current_version, limiter
 from application.models import Cards, Sets
-from application.schemas import (cards_schema, set_with_cards_schema,
-                                 sets_schema, sets_with_cards_schema)
+from application.schemas import (cards_schema, set_with_cards_schema, sets_schema, sets_with_cards_schema)
 
 sets = Blueprint('sets', __name__)
 
