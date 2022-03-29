ba225868e46aea8cf829254ddec76b1d74e0f05b
jmoo <pauperbattlebox@gmail.com>
2022-03-28 21:14:29 -0500

    updated links and static file

--- application/core/routes.py
+++ application/core/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask import current_app as app
 from flask import render_template
+from flask import send_file
 
 from application import current_version
 
@@ -10,6 +11,10 @@ core = Blueprint("core", __name__)
 @core.route("/")
 def home():
     return render_template("index.html")
+
+@core.route("/postman")
+def postman():
+    return send_file("static/MVB.postman_collection.json", as_attachment=True)
 
 
 ####AUTHENTICATE
