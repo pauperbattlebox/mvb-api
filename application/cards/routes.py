9a4404bacde6e3aa37cb52f4da1ee60b6d355cc0
jmoo <pauperbattlebox@gmail.com>
2022-07-01 20:15:21 -0500

    all_cards redirect

--- application/cards/routes.py
+++ application/cards/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, abort
 from flask import current_app as app
-from flask import jsonify, request
+from flask import jsonify, request, redirect
 from marshmallow.exceptions import ValidationError
 
 from application import cache, current_version, limiter
@@ -38,18 +38,7 @@ def get_cache():
 @cache.cached(timeout=86400)
 def get_all_ids():
 
-    q = Meta.query.order_by(Meta.last_updated.desc()).first_or_404()
-
-    result = meta_schema.dump(q)
-
-    p = Cards.query.with_entities(
-        Cards.cs_id, Cards.mtgjson_id, Cards.scryfall_id
-    ).all()
-
-    result["cards"] = cards_schema.dump(p)
-
-    return jsonify(result)
-
+    return redirect("https://cdn.multiversebridge.com/all_ids.json", code=302)
 
 ####SEARCH BY CARD NAME
 @cards.route(current_version + "cards/search")
