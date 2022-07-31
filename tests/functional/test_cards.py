aab6148c28f0e4eb1f068c0d352c5bd1e7cb860d
jmoo <pauperbattlebox@gmail.com>
2022-07-31 13:20:21 -0500

    test_cards update

--- tests/functional/test_cards.py
+++ tests/functional/test_cards.py
@@ -7,8 +7,6 @@ def test_index():
 
     with flask_app.test_client() as test_client:
 
-        # test_client.config.from_pyfile("config.py")
-
         response = test_client.get("/")
 
         assert response.status_code == 200
