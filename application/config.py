c083b5377f07b109a48fe5e12de6cca5c2bc8ca5
jmoo <pauperbattlebox@gmail.com>
2022-07-31 14:34:05 -0500

    config as object

--- application/config.py
+++ application/config.py
@@ -5,9 +5,25 @@ from dotenv import load_dotenv
 basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, ".env"))
 
-SECRET_KEY = environ.get("SECRET_KEY")
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
-RATELIMIT_STORAGE_URI = environ.get("REDISTOGO_URL")
-RATELIMIT_STRATEGY = "moving-window"
-JSON_SORT_KEYS = False
+# SECRET_KEY = environ.get("SECRET_KEY")
+# SQLALCHEMY_TRACK_MODIFICATIONS = False
+# SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+# RATELIMIT_STORAGE_URI = environ.get("REDISTOGO_URL")
+# RATELIMIT_STRATEGY = "moving-window"
+# JSON_SORT_KEYS = False
+
+class Config:
+
+    FLASK_ENV = 'production'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProdConfig(Config):
+
+    SECRET_KEY = environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+    RATELIMIT_STORAGE_URI = environ.get("REDISTOGO_URL")
+    RATELIMIT_STRATEGY = "moving-window"
+    JSON_SORT_KEYS = False
+
+class TestConfig(Config):
+    RATELIMIT_STORAGE_URI = "memory://"
