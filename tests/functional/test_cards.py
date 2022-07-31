8afc9f87a4eaacb4515701d71fd7e2ba5db6a4aa
jmoo <pauperbattlebox@gmail.com>
2022-07-31 13:34:02 -0500

    test_config

--- tests/functional/test_cards.py
+++ tests/functional/test_cards.py
@@ -5,6 +5,8 @@ def test_index():
 
     flask_app = create_app()
 
+    flask_app.config.from_pyfile("config.py")   
+
     with flask_app.test_client() as test_client:
 
         response = test_client.get("/")
