f6f0f6fefc55b3af9a52b08c8cfeea4cf2e4e419
jmoo <pauperbattlebox@gmail.com>
2022-07-31 14:17:13 -0500

    inline config update

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,11 +25,7 @@ def new_set():
 def test_client():
     flask_app = create_app()
 
-    flask_app.config.from_pyfile("test_config.py")
-
-    limiter.init_app(flask_app)
-
-    limiter.enabled = False
+    flask_app.config["RATELIMIT_STORAGE_URI"] = "memory://"    
 
     with flask_app.test_client() as testing_client:
         with flask_app.app_context():
