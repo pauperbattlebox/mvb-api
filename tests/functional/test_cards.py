b3681f4a34d0f7ef340d7dcb830159f61e1756c2
JMoo <pauperbattlebox@gmail.com>
2022-08-05 14:09:06 -0500

    json tests

--- tests/functional/test_cards.py
+++ tests/functional/test_cards.py
@@ -1,3 +1,5 @@
+import requests
+
 def test_index(test_client):
 
     response = test_client.get("/")
@@ -16,4 +18,22 @@ def test_cache(test_client):
 
     response = test_client.get("/api/v1/cache")
 
+    assert response.status_code == 200
     assert b"last_updated" in response.data
+
+def test_all_cards(test_client):
+
+    response = test_client.get("/api/v1/cards")
+    redirect = requests.get("https://cdn.multiversebridge.com/all_ids.json")
+
+    assert response.status_code == 302
+    assert "9cb62c63-8fba-51a5-a8c6-aa788c13d6a5" in redirect.text
+
+def test_mtgjson():
+
+    response = requests.get("https://cdn.multiversebridge.com/mtgjson_build.json")
+
+    print(response.text)
+
+    assert response.status_code == 200
+    assert "15947b20-8c8e-42ed-9599-8b180a382d21" in response.text
