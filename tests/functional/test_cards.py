29cd8108cdaa8a0498a05de8c2a91805269a8f57
JMoo <pauperbattlebox@gmail.com>
2022-08-05 13:32:00 -0500

    test cache

--- tests/functional/test_cards.py
+++ tests/functional/test_cards.py
@@ -11,3 +11,9 @@ def test_cards_post(test_client):
     response = test_client.post("/api/v1/cards/cs/1")
 
     assert response.status_code == 405
+
+def test_cache(test_client):
+
+    response = test_client.get("/api/v1/cache")
+
+    assert b"last_updated" in response.data
